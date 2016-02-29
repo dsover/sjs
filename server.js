c197380c4266299ce689f26cf93bd23f24445224
Alex <alexsover@gmail.com>
2016-02-28 21:22:21 -0500

    t

--- server.js
+++ server.js
@@ -19,6 +19,18 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
+
+app.createRoutes = function() {
+    app.routes = { };
+    app.routes['/'] = function(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+        res.send(fs.readFileSync('./index.html'));
+    };
+};
+app.createRoutes();
+for (var r in app.routes) {
+    app.get(r, app.routes[r]);
+}
 /**
  *  Define the sample application.
  */
