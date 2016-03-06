60250fb43368a5cd9d43c3dfedf5f175ffd8eae8
Alex <alexsover@gmail.com>
2016-03-05 20:21:10 -0500

    dd

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var express = require('express');
 var fs      = require('fs');
 var app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use(express.static('public'));
 app.set('components','./src/components')
