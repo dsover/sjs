3da0773a66672e2829bf4ccb9e3d5d650abdc0c3
Alex <alexsover@gmail.com>
2016-03-06 21:20:14 -0500

    d

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ var mongoose = require('mongoose');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-mongoose.connect('mongodb://'+server_ip_address+'/blog');
+
+var connection_string = '127.0.0.1:27017/blog';
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+mongoose.connect(connection_string);//('mongodb://'+server_ip_address+'/blog');
 
 
 app.use(express.static('public'));
