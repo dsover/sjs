78fa4ff3526d3eb87bb1f63d7cae36345f07e698
Alex <alexsover@gmail.com>
2016-03-06 21:24:09 -0500

    d

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.createRoutes = function() {
             if (err) throw err;
 
             // object of all the users
-            console.log(entry);
+            //console.log(entry);
             res.render('index',{ nav : navigation, footer : footer, post : entry });
         });
     };
@@ -51,18 +51,18 @@ app.createRoutes = function() {
     app.routes['/404'] = function(req, res) {
     res.setHeader('Content-Type', 'text/html');
         res.render('404',{ nav : navigation, footer : footer }); 
-            var Entry = require('./src/models/entry');
-
-            var entry1 = new Entry({
-              title: 'Samual Post',
-              content: "<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing. </p><p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p><p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>"
-            });
-
-            entry1.save(function(err) {
-              if (err) throw err;
-
-              console.log('entry saved successfully!');
-            });
+//            var Entry = require('./src/models/entry');
+//
+//            var entry1 = new Entry({
+//              title: 'Samual Post',
+//              content: "<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing. </p><p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p><p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>"
+//            });
+//
+//            entry1.save(function(err) {
+//              if (err) throw err;
+//
+//              console.log('entry saved successfully!');
+//            });
     };
     
     app.routes['/about'] = function(req, res) {
@@ -84,7 +84,7 @@ app.createRoutes = function() {
             if (err) throw err;
 
             // object of all the users
-            console.log(entries);
+            //console.log(entries);
             res.render('blog-post',{ nav : navigation, footer : footer, post : entries });
         });
         
