9e8e2ba4f480d28308aebebad72dfa42a9066cca
Alex Sover <david.sover@turner.com>
2019-02-08 10:53:12 -0500

    updates

---
+++ src/routes/mailingListRoute.js
@@ -0,0 +1,9 @@
+module.exports = function(app, extras) {
+  app.get('/mailing-list', function(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.render('mailingList', {
+      nav: extras.navigation,
+      footer: extras.footer
+    });
+  });
+};
