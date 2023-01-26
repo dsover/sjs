5001613e3b5ba260dfb8cccc6658733ce498f2b4
dsover <alexsover@gmail.com>
2023-01-25 20:14:27 -0500

    update

---
+++ src/data/dates.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        date: "February 24 - 26",
+        sup:"th",
+        eventName:"ATL ComicCon",
+        type:"Vendor",
+        link:"https://atlcomicconvention.com/"
+    },
+    {
+        date: "April 21 - 23",
+        sup:"rd",
+        eventName:"JordanCon",
+        type:"Guest",
+        link:"https://www.jordancon.org/"
+    },
+    {
+        date: "September 1 - 3",
+        sup:"rd",
+        eventName:"Dragon Con",
+        type:"Attendee",
+        link:"https://www.dragoncon.org/"
+    }
+];
--- src/routes/homeRoute.js
+++ src/routes/homeRoute.js
@@ -1,34 +1,15 @@
+const dates = require('../data/dates');
+const links = require('../data/links');
+const panels = require('../data/panels');
 module.exports = function(app, extras) {
   app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/html');
-    dates = [
-      {
-        date: "February 24 - 26",
-        sup:"th",
-        eventName:"ATL ComicCon",
-        type:"Vendor",
-        link:"https://atlcomicconvention.com/"
-      },
-      {
-        date: "April 21 - 23",
-        sup:"rd",
-        eventName:"JordanCon",
-        type:"Guest",
-        link:"https://www.jordancon.org/"
-      },
-      {
-        date: "September 1 - 3",
-        sup:"rd",
-        eventName:"Dragon Con",
-        type:"Attendee",
-        link:"https://www.dragoncon.org/"
-      }
-    ]
-
     res.render('index', {
       nav: extras.navigation,
       footer: extras.footer,
-      dates: dates
+      dates: dates,
+      links: links,
+      panels: panels
     });
   });
 };
