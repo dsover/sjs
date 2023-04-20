e45b965b499c2ebb13aca852cbf202b58d1dc0cb
dsover <alexsover@gmail.com>
2023-04-19 20:43:04 -0400

    updates

--- src/data/dates.js
+++ src/data/dates.js
@@ -1,12 +1,5 @@
 module.exports = [
     {
-        date: "February 24 - 26",
-        sup:"th",
-        eventName:"ATL ComicCon",
-        type:"Vendor",
-        link:"https://atlcomicconvention.com/"
-    },
-    {
         date: "April 21 - 23",
         sup:"rd",
         eventName:"JordanCon",
@@ -14,10 +7,38 @@ module.exports = [
         link:"https://www.jordancon.org/"
     },
     {
+        date: "May 20 - 22",
+        sup:"nd",
+        eventName:"Georgia Renaissance Festival",
+        type:"Vendor",
+        link:"https://www.garenfest.com/"
+    },
+    {
+        date: "June 2 - 4",
+        sup:"th",
+        eventName:"ConCarolinas",
+        type:"Guest",
+        link:"https://concarolinas.org/"
+    },
+    {
+        date: "July 14 - 16",
+        sup:"th",
+        eventName:"ConGregate",
+        type:"Guest",
+        link:"http://con-gregate.com/"
+    },
+    {
         date: "September 1 - 3",
         sup:"rd",
         eventName:"Dragon Con",
         type:"Attendee",
         link:"https://www.dragoncon.org/"
+    },
+    {
+        date: "October 20 - 22",
+        sup:"nd",
+        eventName:"Multiverse",
+        type:"Guest",
+        link:"https://www.multiversecon.org/"
     }
 ];
