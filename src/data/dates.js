c2d355a0957ad9175c59542f1d6d10324d061fdf
David Sover <alexsover@gmail.com>
2023-10-03 21:05:50 -0400

    updates for site

--- src/data/dates.js
+++ src/data/dates.js
@@ -1,50 +1,5 @@
 module.exports = [
     {
-        date: "April 21 - 23",
-        sup:"rd",
-        year: "2023",
-        eventName:"JordanCon",
-        type:"Guest",
-        location:"Atlanta, GA",
-        link:"https://www.jordancon.org/"
-    },
-    {
-        date: "May 20 - 22",
-        sup:"nd",
-        year: "2023",
-        eventName:"Georgia Renaissance Festival",
-        type:"Vendor",
-        location:"Atlanta, GA",
-        link:"https://www.garenfest.com/"
-    },
-    {
-        date: "June 2 - 4",
-        sup:"th",
-        year: "2023",
-        eventName:"ConCarolinas",
-        type:"Guest",
-        location:"Charlotte, NC",
-        link:"https://concarolinas.org/"
-    },
-    {
-        date: "July 14 - 16",
-        sup:"th",
-        year: "2023",
-        eventName:"ConGregate",
-        type:"Guest",
-        location: 'Winston-Salem, NC',
-        link:"http://con-gregate.com/"
-    },
-    {
-        date: "September 1 - 3",
-        sup:"rd",
-        year: "2023",
-        eventName:"Dragon Con",
-        type:"Attendee",
-        location:"Atlanta, GA",
-        link:"https://www.dragoncon.org/"
-    },
-    {
         date: "October 20 - 22",
         sup:"nd",
         year: "2023",
@@ -52,5 +7,14 @@ module.exports = [
         type:"Guest",
         location:"Atlanta, GA",
         link:"https://www.multiversecon.org/"
+    },
+    {
+        date: "Nov 17 - 19",
+        sup:"th",
+        year: "2023",
+        eventName:"CONjuration",
+        type:"Guest",
+        location:"Atlanta, GA",
+        link:"https://www.conjurationcon.com/"
     }
 ];
