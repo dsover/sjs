29350ccb801a70ce213c73653cdb0bcc14076a34
David Sover <alexsover@gmail.com>
2023-11-30 10:43:36 -0500

    updates for site

--- src/data/dates.js
+++ src/data/dates.js
@@ -1,20 +1,38 @@
 module.exports = [
     {
-        date: "October 20 - 22",
-        sup:"nd",
-        year: "2023",
-        eventName:"Multiverse",
-        type:"Guest",
+        date: "Feb 9 - 11",
+        sup:"th",
+        year: "2024",
+        eventName:"ATL ComicCon",
+        type:"Vendor",
         location:"Atlanta, GA",
-        link:"https://www.multiversecon.org/"
+        link:"https://atlcomicconvention.com/"
     },
     {
-        date: "Nov 17 - 19",
+        date: "Apr 6",
         sup:"th",
-        year: "2023",
-        eventName:"CONjuration",
+        year: "2024",
+        eventName:"Crazy Book Lady Event",
+        type:"Participant",
+        location:"Acworth, GA",
+        link:"https://www.thecrazybookladyga.com/"
+    },
+    {
+        date: "Apr 19 - 21",
+        sup:"st",
+        year: "2024",
+        eventName:"JordanCon",
         type:"Guest",
         location:"Atlanta, GA",
-        link:"https://www.conjurationcon.com/"
-    }
+        link:"https://www.jordancon.org/"
+    },
+    {
+        date: "Aug 29 - Sept 2",
+        sup:"nd",
+        year: "2024",
+        eventName:"DragonCon",
+        type:"Attendee",
+        location:"Atlanta, GA",
+        link:"https://www.dragoncon.org/"
+    },
 ];
